6ba3890a2918869a3e723d111b838cc1291cb9f0
Harsh Shukla <harshshukla@Harshs-MacBook-Air.local>
2024-07-28 18:47:05 +0530

    Database changes

--- todoApp/database.py
+++ todoApp/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-postgreSQL_DATABASE_URL = "postgresql://harshshukla:test1234!@localhost:5433/TodoApplicationDatabase"
+postgreSQL_DATABASE_URL = "postgresql://harshshukla:8U3OJzWgCU98eFjDbiHgF6rLJWU3NjMo@dpg-cqj460aj1k6c739mdgug-a.oregon-postgres.render.com/todoapplicationdatabase_ehq2"
 
 engine = create_engine(postgreSQL_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
